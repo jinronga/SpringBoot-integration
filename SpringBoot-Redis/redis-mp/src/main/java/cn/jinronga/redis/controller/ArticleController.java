d61a445f2ae4a33b98abb25a55ef12bbb7628e06
guojr <guojr@hua-cloud.com.cn>
2021-12-18 11:26:42 +0800

    增加：数据类型HyperLogLog，Bitmap，Geo数据类型实战

--- SpringBoot-Redis/redis-mp/src/main/java/cn/jinronga/redis/controller/ArticleController.java
+++ SpringBoot-Redis/redis-mp/src/main/java/cn/jinronga/redis/controller/ArticleController.java
@@ -1,21 +1,23 @@
 package cn.jinronga.redis.controller;
 
+import cn.jinronga.redis.service.ArticleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
+
 
 @RestController
 @Slf4j
-@Api(description = "喜欢的文章接口")
+@Api(value = "喜欢的文章接口")
 public class ArticleController
 {
-    @Resource
+    @Autowired
     private ArticleService articleService;
 
     @ApiOperation("喜欢的文章，点一次加一个喜欢")
